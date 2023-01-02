0570eb2eb28871765e8ba413c6a7a0fc1a9a9b6d
Dylan Shine <dylan@shinelabs.dev>
2023-01-02 13:34:58 -0700

    Set default usage tokens from -1 to 0

--- Sources/OpenAIKit/Usage.swift
+++ Sources/OpenAIKit/Usage.swift
@@ -9,8 +9,8 @@ public struct Usage {
 extension Usage: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.promptTokens = try container.decodeIfPresent(Int.self, forKey: .promptTokens) ?? -1
-        self.completionTokens = try container.decodeIfPresent(Int.self, forKey: .completionTokens) ?? -1
-        self.totalTokens = try container.decodeIfPresent(Int.self, forKey: .totalTokens) ?? -1
+        self.promptTokens = try container.decodeIfPresent(Int.self, forKey: .promptTokens) ?? 0
+        self.completionTokens = try container.decodeIfPresent(Int.self, forKey: .completionTokens) ?? 0
+        self.totalTokens = try container.decodeIfPresent(Int.self, forKey: .totalTokens) ?? 0
     }
 }
