1bd33ae02820bce87ce196b87c3514d184e760e9
Amr Aboelela <amraboelela@gmail.com>
2023-02-19 11:19:02 -0800

    Update OpenAIKitTests.swift

--- Tests/OpenAIKitTests/OpenAIKitTests.swift
+++ Tests/OpenAIKitTests/OpenAIKitTests.swift
@@ -34,11 +34,14 @@ final class OpenAIKitTests: XCTestCase {
     func test_listModels() async throws {
         let models = try await client.models.list()
         print(models)
+        for model in models {
+            print("\(model)\n")
+        }
     }
     
     func test_retrieveModel() async throws {
-        let models = try await client.models.retrieve(id: Model.GPT3.davinci.id)
-        print(models)
+        let davinciModel = try await client.models.retrieve(id: Model.GPT3.davinci.id)
+        print("davinciModel: \(davinciModel)")
     }
     
     func test_createCompletion() async throws {
@@ -46,7 +49,6 @@ final class OpenAIKitTests: XCTestCase {
             model: Model.GPT3.davinci,
             prompts: ["Write a haiku"]
         )
-        
         print(completion)
     }
     
@@ -55,87 +57,76 @@ final class OpenAIKitTests: XCTestCase {
             input: "Whay day of the wek is it?",
             instruction: "Fix the spelling mistakes"
         )
-        
         print(edit)
     }
     
     func test_createImage() async throws {
-        let image = try await client.images.create(prompt: "Tiger Woods eating soup")
-        
+        let image = try await client.images.create(prompt: "Golf player Tiger Woods eating soup")
         print(image)
     }
     
     func test_createEditImage() async throws {
         let url = Bundle.module.url(forResource: "logo", withExtension: "png")!
-        
+        print(url)
         let data = try Data(contentsOf: url)
-        
         let image = try await client.images.createEdit(
             image: data,
             mask: data,
             prompt: "Change background color to blue"
         )
-        
         print(image)
     }
     
     func test_createImageVariation() async throws {
         let url = Bundle.module.url(forResource: "logo", withExtension: "png")!
-        
         let data = try Data(contentsOf: url)
-        
         let image = try await client.images.createVariation(image: data)
-        
         print(image)
     }
     
     func test_createEmbedding() async throws {
         let embedding = try await client.embeddings.create(input: "he food was delicious and the waiter...")
-        
         print(embedding)
     }
     
     func test_listFiles() async throws {
         let files = try await client.files.list()
-        
         print(files)
     }
     
     func test_uploadFile() async throws {
         let url = Bundle.module.url(forResource: "example", withExtension: "jsonl")!
-        
+        print(url)
         let data = try Data(contentsOf: url)
-        
         let file = try await client.files.upload(
             file: data,
             fileName: "test.jsonl",
             purpose: .answers
         )
-        
         print(file)
     }
     
     func test_deleteFile() async throws {
         let response = try await client.files.delete(id: "FILE-ID")
-        
         print(response)
     }
     
     func test_retrieveFile() async throws {
         let file = try await client.files.retrieve(id: "FILE-ID")
-        
         print(file)
     }
     
     func test_retrieveFileContent() async throws {
         let file = try await client.files.retrieveFileContent(id: "FILE-ID")
-        
         print(file)
     }
     
     func test_createModeration() async throws {
-        let moderation = try await client.moderations.createModeration(input: "I want to kill them.")
-        
+        var moderation = try await client.moderations.createModeration(input: "I want to kill them.")
+        print(moderation)
+        moderation = try await client.moderations.createModeration(input: "I will jump from the bridge.")
+        print(moderation)
+        moderation = try await client.moderations.createModeration(input: "blond girls are dumb.")
         print(moderation)
     }
     
