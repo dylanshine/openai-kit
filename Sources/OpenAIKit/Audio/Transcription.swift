ea237019018bade99f1daf821ac4ef27e13ad8b9
Joshua Galvan <joshegalv@gmail.com>
2023-06-12 15:09:46 -0500

    Implement Transcription and Translation types

---
+++ Sources/OpenAIKit/Audio/Transcription.swift
@@ -0,0 +1,14 @@
+//
+//  Transcription.swift
+//
+//
+//  Created by Joshua Galvan on 6/12/23.
+//
+
+import Foundation
+
+struct Transcription {
+    public let text: String
+}
+
+extension Translation: Codable {}
---
+++ Sources/OpenAIKit/Audio/Translation.swift
@@ -0,0 +1,14 @@
+//
+//  Transcription.swift
+//
+//
+//  Created by Joshua Galvan on 6/12/23.
+//
+
+import Foundation
+
+struct Translation {
+    public let text: String
+}
+
+extension Translation: Codable {}
