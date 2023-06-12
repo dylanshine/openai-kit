f26c185a2cc9a7e6031b990fdd01ae1b210dbe7d
Joshua Galvan <joshegalv@gmail.com>
2023-06-12 16:26:27 -0500

    Add .m4a to MIMEType.Audio

--- Sources/OpenAIKit/MIMEType.swift
+++ Sources/OpenAIKit/MIMEType.swift
@@ -1,6 +1,6 @@
 //
 //  MIMEType.swift
-//  
+//
 //
 //  Created by Dylan Shine on 3/19/23.
 //
@@ -8,18 +8,19 @@
 import Foundation
 
 public enum MIMEType {
-    
+
     public enum File: String {
         case json = "application/json"
     }
-    
+
     public enum Audio: String {
         case mpeg = "audio/mpeg"
         case mp4 = "audio/mp4"
         case wav = "audio/wav"
         case webm = "audio/webm"
+        case m4a = "audio/m4a"
     }
-    
+
 }
 
 extension MIMEType.File: Codable {}
