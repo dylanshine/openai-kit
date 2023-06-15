bf4e81668fe3c561afa29078121f3ff7d99da210
Aleksei Cherepanov <ftp27host@gmail.com>
2023-06-15 18:39:06 +0300

    Path prefix to proxy config added (#59)

--- Sources/OpenAIKit/RequestHandler/RequestHandler.swift
+++ Sources/OpenAIKit/RequestHandler/RequestHandler.swift
@@ -12,7 +12,9 @@ extension RequestHandler {
         var components = URLComponents()
         components.scheme = configuration.api?.scheme.value ?? request.scheme.value
         components.host = configuration.api?.host ?? request.host
-        components.path = request.path
+        components.path = [configuration.api?.path, request.path]
+            .compactMap { $0 }
+            .joined()
             
         guard let url = components.url else {
             throw RequestHandlerError.invalidURLGenerated
